af6cc41a97afcfd8aebed9c845bc09877d2c4ef7
ocube-leroy <olagokeolowu@gmail.com>
2021-09-13 17:49:32 +0800

    modified3 assignment3

--- assignment3.js
+++ assignment3.js
@@ -1,8 +1,41 @@
-function taskThree(number4, number5, number6){
+function taskThree(a, b, c) {
 
-	return number4 - number5 + number6;
+	if (a < b && b < c) {
 
-}
-const numero3 = taskThree(12, 9, 28);
+	return (b - a + c);
+
+	}	
+	
+	else if (a < c && c < b) {
+
+	return (c - a + b);
+
+	}	
+
+	else if (b < a && a < c) {
+
+	return (a - b + c);
+
+	}	
+
+	else if (b < c && c < a) {
+	
+	return (c - b + a);
+
+	}	
+
+	else if (c < a && a < b) {
+
+	return (a - c + b);
+
+	}	
+
+	else if (c < b && b < a);
+
+	return (b - c + a);
+
+	}
+	
+	const numero3 = taskThree(12, 9, 28);
 
 console.log(numero3);
