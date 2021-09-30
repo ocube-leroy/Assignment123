c771a41f9b0683243be2cc069ba558ff2e051385
ocube-leroy <olagokeolowu@gmail.com>
2021-09-30 16:48:21 +0800

    relocate arrays file

---
+++ arrays.js
@@ -0,0 +1,13 @@
+
+    function arrayToAccept(_parameter){
+        const myArray = ["yellow", "blue", "purple", "lilac", "peach", "red", "black"];
+        const lengthOfArray = myArray.length;
+        const middleArrayIndex = ((myArray.length-1)/2);
+        const middleArray = myArray[middleArrayIndex];
+
+    console.log(lengthOfArray);
+    console.log(middleArrayIndex);
+    console.log(middleArray);
+}
+    
+    arrayToAccept("yellow", "blue", "purple", "lilac", "peach", "red", "black");
